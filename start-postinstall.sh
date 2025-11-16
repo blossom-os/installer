d7765c2d0b6f5419d476abc18eb7d626053e9158
koyu <me@koyu.space>
2025-11-16 08:08:51 +0100

    feat: refactor panel handling in start-postinstall.sh to use desktopContainments for better management

--- start-postinstall.sh
+++ start-postinstall.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 qdbus6 org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript '
-var ids = panelIds();
-for (var i = 0; i < ids.length; i++) {
-    var p = panelById(ids[i]);
-    // Panel komplett verstecken, wenn möglich:
-    p.height = 0;
-    // Oder zumindest autohide einstellen:
-    p.hiding = "autohide";
+var all = desktopContainments();
+for (var i = 0; i < all.length; i++) {
+    var c = all[i];
+    if (c.type === "Panel") {
+        c.height = 0;
+        // c.hiding = "autohide";
+    }
 }
 '
 cd /opt/blossomos-installer
