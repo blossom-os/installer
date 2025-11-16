78de1c471d0b1cad35c16af77e2ac705e5a3c57a
koyu <me@koyu.space>
2025-11-16 08:11:01 +0100

    feat: refactor panel management in start-postinstall.sh to use awk for configuration updates

--- start-postinstall.sh
+++ start-postinstall.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-qdbus6 org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript '
-var all = desktopContainments();
-for (var i = 0; i < all.length; i++) {
-    var c = all[i];
-    if (c.type === "Panel") {
-        c.height = 0;
-        // c.hiding = "autohide";
-    }
-}
-'
+CONFIG="$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc"
+BACKUP="$CONFIG.bak"
+
+cp "$CONFIG" "$BACKUP"
+echo "Backup erstellt: $BACKUP"
+
+awk '
+  /^\[Containments/ {in_panel=0}
+  /plugin=panel/ {in_panel=1; next}
+  !in_panel {print}
+' "$CONFIG" > "${CONFIG}.tmp" && mv "${CONFIG}.tmp" "$CONFIG"
+
+killall plasmashell
+sleep 1
+plasmashell &
+
 cd /opt/blossomos-installer
-/home/$USER/.bun/bin/bun dev --postinstall
+/home/$USER/.bun/bin/bun dev
