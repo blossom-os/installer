2ca83483c8fd17e5366f330a08ad82a0a3452d56
koyu <me@koyu.space>
2025-11-16 08:11:41 +0100

    feat: remove backup and awk processing for plasma configuration in start-postinstall.sh

--- start-postinstall.sh
+++ start-postinstall.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-CONFIG="$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc"
-BACKUP="$CONFIG.bak"
-
-cp "$CONFIG" "$BACKUP"
-echo "Backup erstellt: $BACKUP"
-
-awk '
-  /^\[Containments/ {in_panel=0}
-  /plugin=panel/ {in_panel=1; next}
-  !in_panel {print}
-' "$CONFIG" > "${CONFIG}.tmp" && mv "${CONFIG}.tmp" "$CONFIG"
-
 killall plasmashell
-sleep 1
-plasmashell &
-
 cd /opt/blossomos-installer
 /home/$USER/.bun/bin/bun dev
